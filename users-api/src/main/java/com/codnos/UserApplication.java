0cf11f5ee7ea391d880c904ad5522c9265c53f5b
ligasgr <ligasgr@gmail.com>
2018-04-01 22:20:29 +0100

    Setup maven projects to serve static content in api webflux app.

--- users-api/src/main/java/com/codnos/UserApplication.java
+++ users-api/src/main/java/com/codnos/UserApplication.java
@@ -2,11 +2,24 @@ package com.codnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
 
 @SpringBootApplication
 public class UserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> routingFunction() {
+        return resources("/webjars/**", new ClassPathResource("classpath:/META-INF/resources/webjars/"))
+                .and(resources("/**", new ClassPathResource("public/")));
+    }
 }
