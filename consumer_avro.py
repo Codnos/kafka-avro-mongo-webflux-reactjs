0a84b92acd5c8532f948f148003d1a8dadfd6a80
ligasgr <ligasgr@gmail.com>
2019-06-09 21:17:36 +0100

    Add decoding from protobuf into full object.

--- consumer_avro.py
+++ consumer_avro.py
@@ -1,8 +1,24 @@
 import io
 import urllib.request
-import json
 import pandas as pd
 import avro.schema
+import avro.io
+import decimal
+
+
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val                         # return positive value as is
+
+def decimal_from_bytes(bytes, scale):
+    original = int.from_bytes(bytes, byteorder='big', signed=False)
+    bits = len(bytes)*8
+    decoded = twos_comp(original, bits)
+    return decimal.Decimal(decoded) / decimal.Decimal(10**scale)
+
+
 
 url = "http://localhost:8080/api/users"
 headers = {"Accept": "application/avro"}
@@ -17,3 +33,18 @@ decoder = avro.io.BinaryDecoder(bytes_reader)
 reader = avro.io.DatumReader(schema)
 user = reader.read(decoder)
 print(user)
+
+columns = user['salary_structure']
+extended = ['place'] + columns
+scale = user['salary_precision']
+rows = []
+for key, value in user['salaries'].items():
+    numbers = []
+    for val in value:
+        numbers.append(decimal_from_bytes(val, scale))
+    row = [key] + numbers
+    rows.append(row)
+
+df = pd.DataFrame(rows, columns=(extended))
+
+print(df)
